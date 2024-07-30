150ab52e82b20e30d57da4ca4a94b5964c582ec4
0x4E43 <nimaic.dev@gmail.com>
2024-07-30 20:48:34 +0530

    Code Cleanup

--- dummy-service/src/main/java/com/web/dummy/DummyServiceApplication.java
+++ dummy-service/src/main/java/com/web/dummy/DummyServiceApplication.java
@@ -1,21 +1,15 @@
 package com.web.dummy;
 
-import com.web.dummy.websockets.vo.Greeting;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DummyServiceApplication {
 
-	@Autowired
-	private SimpMessagingTemplate messagingTemplate;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DummyServiceApplication.class, args);
@@ -27,9 +21,5 @@ public class DummyServiceApplication {
 	}
 
 
-	@PostMapping("/add")
-	public String addEvent(@RequestBody Greeting greeting) {
-		messagingTemplate.convertAndSend("/topic/greetings/"+greeting.getTenant(), greeting);
-		return "Event added";
-	}
+
 }
--- dummy-service/src/main/java/com/web/dummy/websockets/controller/WebSocketController.java
+++ dummy-service/src/main/java/com/web/dummy/websockets/controller/WebSocketController.java
@@ -2,19 +2,32 @@ package com.web.dummy.websockets.controller;
 
 import com.web.dummy.websockets.vo.Greeting;
 import com.web.dummy.websockets.vo.HelloMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class WebSocketController {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
     
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(1000); // simulated delay
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+    @PostMapping("/add")
+    public String addEvent(@RequestBody Greeting greeting) {
+        messagingTemplate.convertAndSend("/topic/greetings/"+greeting.getTenant(), greeting);
+        return "Event added";
     }
 
 }
